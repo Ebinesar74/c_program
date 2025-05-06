1c6f7a56cbfe495a37ae8005f31f0ba240b1e8a1
Ebinesar74 <e23bm009@shanmugha.edu.in>
2025-05-06 11:35:23 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+    //Declare variables
+    int i=0;
+    printf("Character\t ASCII valve");
+    //print ASCII valves
+    for (i=65;i<=122;i++) {
+    printf("%c \t\t %d",i, i);
+}
+return 0;
+}
