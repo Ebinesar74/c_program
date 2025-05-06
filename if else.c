3264998b1baf2fccd3bde52ed1b400454721ed24
Ebinesar74 <e23bm009@shanmugha.edu.in>
2025-05-06 14:34:52 +0530

    Create if else.c

---
+++ if else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+ int number = 5;
+ 
+ if( number < 0) {
+     printf("the number is non-negative.\n");
+ }
+ else
+     printf("the number is is negative.\n");
+     
+ return 0;
+ }
